c6ac0a43519c59301bbf0c5817d2e198dc5111da
Alan Oliver <alrobwilloliver@gmail.com>
2020-06-05 17:34:40 +0100

    post bug fix

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.get('/leaderboard', async (req, res) => {
     res.render('pages/leaderboard', { scores })
 })
 
-app.post('/api/newScore', async (req, res) => {
+app.post('api/newScore', async (req, res) => {
     console.log(req.body);
     const score = new Score({ name: req.body.name, score: req.body.score });
 
